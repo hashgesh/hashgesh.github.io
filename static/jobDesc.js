f8c0953162642975d0b7b9d8a7b006f919bcc546
Arif Kursad KAVAS <arif.kavas@sunhill-technologies.com>
2020-08-28 12:16:01 +0200

    Css

--- static/jobDesc.js
+++ static/jobDesc.js
@@ -19,12 +19,11 @@ export const bimDesc = [
 ];
 
 export const nokiaDesc = [
-  'Developed software for GSM network backbone. Multiprocess, multithreaded scalable\
-applications realized on Linux servers with strict performance considerations. Extensive usage of \
-C/C++, for development of mediation framework with concentration on High Available File\
-Systems, Scalability, synchronization, network programming. Used Vi and Eclipse for\
+  'Developed software for GSM network backbone',
+  'Multiprocess, multithreaded scalable applications realized on Linux servers with strict performance considerations.',
+  'Extensive usage of C/C++, for development of mediation framework with concentration on High Available File Systems, Scalability, synchronization, network programming. Used Vi and Eclipse for\
 development and ddd/gdb for debugging, Valgrind for memory tests',
-  'Worked on devops pipeline of the software projects. Writing and maintaining tools for\
+  'Worked on devops pipeline of the software projects. Writing and maintaining tools for \
 continuous integration',
   'Prepared high and low level design documentation',
   'Requirement negotiation meetings on customer sites(Attended these meetings as project\
