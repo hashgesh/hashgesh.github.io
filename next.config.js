f550df893338cee3f7c09e2463097e3f33a401a3
hashgesh <arifkavas@gmail.com>
2021-04-05 20:34:54 +0200

    Nextjs porting

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = withCss(withSass({
                     limit: 100000
                 }
             },
-            distDir: '/',
+            distDir: './',
         });
 
         return config;
