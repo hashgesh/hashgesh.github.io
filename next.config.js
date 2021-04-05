42b0061a426f03bdcb2abc5bf9de1ec71fbab941
hashgesh <arifkavas@gmail.com>
2021-04-05 20:35:22 +0200

    Nextjs porting

--- next.config.js
+++ next.config.js
@@ -11,7 +11,7 @@ module.exports = withCss(withSass({
                     limit: 100000
                 }
             },
-            distDir: './',
+            assetPrefix: 'next',
         });
 
         return config;
